d25cc7a45aa339f820e6c88e7ab6ded1f518cb6e
Kaisa Hakola <kaisahakola00@gmail.com>
2026-01-16 18:08:23 +0200

    Add search bar feature to library and delete unused components
    Replace the SearchBar component that was only a placeholder component. Plus remove BookList component and move its implementation directly to the library screen. Remove BookList.tsx and SearchBar.tsx files.

--- app/(tabs)/library/index.tsx
+++ app/(tabs)/library/index.tsx
@@ -1,13 +1,56 @@
-import { StyleSheet } from 'react-native';
-import BookList from '@/components/BookList';
+import { FlatList, StyleSheet, TextInput, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import SearchBar from '@/components/SearchBar';
+import { useFetchAllBooks } from '@/hooks/useFetchAllBooks';
+import React, { useEffect, useState } from 'react';
+import { BookWithIdAndDate } from '@/types/book';
+import BookItem from '@/components/BookItem';
+import { Feather } from '@expo/vector-icons';
 
 const Library = () => {
+  const { books } = useFetchAllBooks();
+  const [searchQuery, setSearchQuery] = useState('');
+  const [sortedBooks, setSortedBooks] = useState<BookWithIdAndDate[]>([]);
+
+  useEffect(() => {
+    if (!books) return;
+    const booksSorted = [...books].sort(sortBooksByDate);
+    setSortedBooks(booksSorted);
+  }, [books]);
+
+  const sortBooksByDate = (a: BookWithIdAndDate, b: BookWithIdAndDate) => {
+    const dateA = new Date(a.createdAt ?? 0).getTime();
+    const dateB = new Date(b.createdAt ?? 0).getTime();
+
+    return dateB - dateA;
+  };
+
+  const filteredBooks = sortedBooks.filter((book) => {
+    const query = searchQuery.toLowerCase().trim();
+    if (!query) return true;
+
+    return (
+      book.title.toLowerCase().includes(query) ||
+      book.author.toLowerCase().includes(query)
+    );
+  });
+
   return (
     <SafeAreaView style={styles.container}>
-      <SearchBar />
-      <BookList />
+      <View style={styles.searchContainer}>
+        <Feather name="search" size={20} color="#66666E" />
+        <TextInput
+          placeholder="Search by title or author"
+          placeholderTextColor="#66666E"
+          value={searchQuery}
+          onChangeText={setSearchQuery}
+        />
+      </View>
+      <FlatList
+        data={filteredBooks}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <BookItem book={item} />}
+        contentContainerStyle={{ paddingBottom: 90 }}
+      />
     </SafeAreaView>
   );
 };
@@ -20,6 +63,27 @@ const styles = StyleSheet.create({
   header: {
     fontFamily: 'Marcellus',
     fontSize: 32,
+  },
+  searchContainer: {
+    backgroundColor: 'white',
+    borderRadius: 15,
+    padding: 12,
+    width: '92%',
+    marginHorizontal: 'auto',
+    marginBottom: 10,
+    flexDirection: 'row',
+    gap: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
+
+    // iOS shadow
+    shadowColor: '#9999A1',
+    shadowOffset: { width: 0, height: 4 },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+
+    // Android shadow
+    elevation: 4,
   },
 });
 
